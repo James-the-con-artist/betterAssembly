578f181bd72ed1f1001e3b9e71f028f213870a16
Johnston Liu <johnstonliu2004@gmail.com>
2020-07-13 15:34:13 -0700

    better implementation of sheets backend

--- scripts/spreadsheet.js
+++ scripts/spreadsheet.js
@@ -1,54 +1,7 @@
-//Currently I have got this program working so that the after running this code in Node it will update the index.ejs file to have Hello, <A2> according to the spreadsheet.
+var spreadsheet = "https://docs.google.com/spreadsheets/d/1SLJXzaJfHwr0WrAu8OvtAmxP1d0NCMyv_O7zZ5YigsE/edit#gid=0";
 
-
-
-var app = require("express")(); 
-var bodyParser = require("body-parser"); 
-
-const { GoogleSpreadsheet } = require('google-spreadsheet');
-const creds = require('../client_secret.json');
-
-const doc = new GoogleSpreadsheet('1SLJXzaJfHwr0WrAu8OvtAmxP1d0NCMyv_O7zZ5YigsE');
-accessSpreadsheet();
-
-//Set view engine to ejs
-app.set("view engine", "ejs"); 
-
-//Tell Express where we keep our index.ejs
-app.set("views", __dirname + "/views"); 
-
-//Use body-parser
-app.use(bodyParser.urlencoded({ extended: false })); 
-
-//Instead of sending Hello World, we render index.ejs
-app.get("/", (req, res) => { res.render("index", { info: number }); });
-
-app.listen(8080, () => { console.log("Server online on http://localhost:8080"); });
-
-function printStuff(a) {
-    console.log(a.Alpha);
-    console.log(a.Beta);
-    console.log('------------');
-}
-
-async function accessSpreadsheet() {
-    await doc.useServiceAccountAuth({
-        client_email: creds.client_email,
-        private_key: creds.private_key,
+$(document).ready(function(){
+    $('#statistics').sheetrock({
+        url: spreadsheet
     });
-    
-    await doc.loadInfo();
-    
-    const sheet = doc.sheetsByIndex[0];
-
-    const rows = await sheet.getRows();
-
-    //The variable that goes in the html lol
-    number = rows[0].Alpha;
-
-    /*
-    rows.forEach(row => {
-        printStuff(row);
-    })
-    */
-}
+});
